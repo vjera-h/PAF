7810ba2c63da144f0f455bf5650ef0658388b773
vjera-h <vhinic@pmfst.hr>
2026-06-03 16:39:27 +0200

    Dovrseno

--- vjezbe5/arithm.py
+++ vjezbe5/arithm.py
@@ -1,6 +1,7 @@
 # Napišite program arithm.py koji računa aritmetičku sredinu i 
 # standardnu devijaciju za 10 točaka.
-import math 
+import numpy as np
+import statistics as st
 
 brojevi = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
@@ -11,8 +12,13 @@ def izračun(brojevi):
     #standardna devijacija
     suma = sum((b - aritmeticka_sredina)**2 for b in brojevi)
     nazivnik = n * (n - 1)
-    standardna_devijacija = math.sqrt(suma/nazivnik)
+    standardna_devijacija = np.sqrt(suma/nazivnik)
 
     print(f'Aritmetička sredina je: {aritmeticka_sredina}\nStandardna devijacija je: {standardna_devijacija}')
 
+print('Program koji račina:')
 izračun(brojevi)
+
+#(b) Napišite program pod (a) koristeći gotove module.
+print('Računanje pomoću gotovih modula')
+print(f'Aritmetička sredina je: {st.mean(brojevi)}\nStandardna devijacija je: {np.std(brojevi)}')
